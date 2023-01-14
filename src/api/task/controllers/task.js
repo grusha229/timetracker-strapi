89cdc5f851db9155580620be344baa6f6fd746bc
Grigory Tupikov <gtupikov@kabanov.agency>
2023-01-14 23:10:36 +0300

    update config for getting tasks

--- src/api/task/controllers/task.js
+++ src/api/task/controllers/task.js
@@ -6,4 +6,37 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::task.task');
+module.exports = createCoreController('api::task.task',() => ({
+  async create(ctx) {
+    const task = ctx
+    task.request.body.data.user = ctx.state.user.id
+
+    const response = await super.create(task);
+
+    return response;
+  },
+
+  async find(ctx) {
+    // some logic here
+
+    let {data, meta} = await super.find(ctx);
+
+    // some more logic
+
+    // console.log('Data',data)
+
+    let filteredData = data.filter((el) => {
+      if (el.attributes.user.data) {
+        if (el.attributes.user.data.id === ctx.state.user.id) {
+          return true
+        }
+      }
+    })
+
+    // console.log('Filtered data',filteredData)
+    data = filteredData
+
+    return {data, meta};
+  },
+
+}));
